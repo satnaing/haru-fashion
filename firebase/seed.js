fbb19a757bd68bfa1fb58a6c25c3dc34cbff818d
Sat Naing <satnaingdev@gmail.com>
2021-05-08 12:51:38 +0630

    test dynamic routes and add firebase folder

---
+++ firebase/seed.js
@@ -0,0 +1,11 @@
+export const seedDatabase = (firebase) => {
+  const products = [
+    {
+      img1: "",
+      img2: "",
+      name: "Bla bla",
+      category: "men",
+      price: 199.99,
+    },
+  ];
+};
