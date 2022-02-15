19ce5b477049c6aa73d39ab134bf2287e9d24f12
Sat Naing <satnaing@Sats-MacBook-Air.local>
2022-02-15 23:53:12 +0630

    i18n: update footer to support 2 languages

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 import FacebookLogo from "../../public/icons/FacebookLogo";
 import InstagramLogo from "../../public/icons/InstagramLogo";
@@ -7,62 +8,63 @@ import Input from "../Input/Input";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
+  const t = useTranslations("Navigation");
+
   return (
     <>
       <div className={styles.footerContainer}>
         <div>
-          <h6 className={styles.footerHead}>COMPANY</h6>
+          <h6 className={styles.footerHead}>{t("company")}</h6>
           <div className={styles.column}>
-            <a href="example">About Us</a>
-            <a href="example">Contact Us</a>
-            <a href="example">Store Location</a>
-            <a href="example">Careers</a>
+            <a href="example">{t("about_us")}</a>
+            <a href="example">{t("contact_us")}</a>
+            <a href="example">{t("store_location")}</a>
+            <a href="example">{t("careers")}</a>
           </div>
         </div>
         <div>
-          <h6 className={styles.footerHead}>HELP</h6>
+          <h6 className={styles.footerHead}>{t("help")}</h6>
           <div className={styles.column}>
-            <a href="example">Order Tracking</a>
-            <a href="example">FAQs</a>
-            <a href="example">Privacy Policy</a>
-            <a href="example">Terms & Conditions</a>
+            <a href="example">{t("order_tracking")}</a>
+            <a href="example">{t("faqs")}</a>
+            <a href="example">{t("privacy_policy")}</a>
+            <a href="example">{t("terms_conditions")}</a>
           </div>
         </div>
         <div>
-          <h6 className={styles.footerHead}>STORE</h6>
+          <h6 className={styles.footerHead}>{t("store")}</h6>
           <div className={styles.column}>
             <Link href={`/product-category/women`}>
-              <a>Women</a>
+              <a>{t("women")}</a>
             </Link>
             <Link href={`/product-category/men`}>
-              <a>Men</a>
+              <a>{t("men")}</a>
             </Link>
             <Link href={`/product-category/bags`}>
-              <a>Bags</a>
+              <a>{t("bags")}</a>
             </Link>
           </div>
         </div>
         <div>
-          <h6 className={styles.footerHead}>KEEP IN TOUCH</h6>
+          <h6 className={styles.footerHead}>{t("keep_in_touch")}</h6>
           <div className={styles.column}>
             <span>
-              Ground Floor, Yadanar Road, <br />
-              Malikha Building, Thingangyun, <br />
-              Yangon
+              {t("address.detail")}
+              <br />
+              {t("address.road")}
+              <br />
+              {t("address.city")}
             </span>
-            <span>+95 95 096 051</span>
+            <span>{t("phone_number")}</span>
             <span>
-              Open All Days <br />
-              9:00 AM ~ 11:00 PM
+              {t("open_all_days")} <br />- {t("opening_hours")}
             </span>
           </div>
         </div>
       </div>
       <div className="flex flex-col items-center pb-16">
-        <h4 className="text-3xl mb-4">Newsletter</h4>
-        <span className="px-6 text-center">
-          Be the first to know about new arrivals, sales & promos!
-        </span>
+        <h4 className="text-3xl mb-4">{t("newsletter")}</h4>
+        <span className="px-6 text-center">{t("newsletter_desc")}</span>
         <div className="mt-5 px-6 flex w-full sm:w-auto flex-col sm:flex-row">
           <Input name="email" type="email" extraClass=" w-full sm:w-auto" />{" "}
           <Button
@@ -74,10 +76,12 @@ export default function Footer() {
       </div>
       <div className={styles.bottomFooter}>
         <span className="ml-6 sm:ml-12 md:ml-20">
-          @2020 Haru. All rights reserved.
+          @2022 Haru. {t("all_rights_reserved")}
         </span>
         <span className="flex items-center mr-6 sm:mr-12 md:mr-20">
-          <span className="hidden sm:block">Follow us on Social Media:</span>{" "}
+          <span className="hidden sm:block">
+            {t("follow_us_on_social_media")}:
+          </span>{" "}
           <a
             href="www.facebook.com"
             aria-label="Facebook Page for Haru Fashion"
