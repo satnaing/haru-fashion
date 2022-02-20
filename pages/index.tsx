dfc2944b0832aab8d820afdcd74c1dd946497603
Sat Naing <satnaingdev@gmail.com>
2022-02-20 16:26:24 +0630

    style: update Card responsive layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext, useEffect, useCallback, useRef } from "react";
 import { GetStaticProps } from "next";
+import Image from "next/image";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
-import { useRouter } from "next/router";
 
-import CartContext from "../context/cart/CartContext";
-import WishlistContext from "../context/wishlist/WishlistContext";
 import { db } from "./../firebase/firebase";
 import useWindowSize from "../components/Util/useWindowSize";
 import Header from "../components/Header/Header";
@@ -14,10 +12,12 @@ import GhostButton from "../components/Buttons/GhostButton";
 import Button from "../components/Buttons/Button";
 import Slideshow from "../components/HeroSection/Slideshow";
 import OverlayContainer from "../components/OverlayContainer/OverlayContainer";
-import Card3 from "../components/Card/Card3";
-import Card5 from "../components/Card/Card5";
+import Card from "../components/Card/Card";
 import TestiSlider from "../components/TestiSlider/TestiSlider";
 import { itemType } from "../context/cart/cart-types";
+
+// /bg-img/ourshop.png
+import ourShop from "../public/bg-img/ourshop.png";
 
 type Props = {
   products: itemType[];
@@ -25,17 +25,26 @@ type Props = {
 
 const Home: React.FC<Props> = ({ products }) => {
   const t = useTranslations("Index");
-  const [totalItems, setTotalItems] = useState(10);
-  const { addOne } = useContext(CartContext);
-  const { addToWishlist } = useContext(WishlistContext);
-  const [viewWidth, setViewWidth] = useWindowSize();
+  const [totalItems, setTotalItems] = useState(8);
+  const [viewWidth] = useWindowSize();
 
   const currentItems = products.slice(0, totalItems);
 
   // Change totalItems to 8 for good layout
-  useEffect(() => {
-    viewWidth <= 1280 ? setTotalItems(8) : setTotalItems(10);
+  const changeTotalItems = useCallback(() => {
+    // if (viewWidth < 992) return;
+    if (viewWidth >= 992 || viewWidth < 576) {
+      totalItems !== 10 && setTotalItems(10);
+    } else if (viewWidth >= 768) {
+      totalItems !== 8 && setTotalItems(8);
+    } else {
+      totalItems !== 9 && setTotalItems(9);
+    }
   }, [viewWidth]);
+
+  useEffect(() => {
+    changeTotalItems();
+  }, [changeTotalItems]);
 
   return (
     <div>
@@ -87,39 +96,18 @@ const Home: React.FC<Props> = ({ products }) => {
           </div>
         </div>
       </section>
-      <section className="w-full h-full flex flex-col justify-center items-center mt-16 mb-20">
-        <div className="w-3/4 sm:w-1/2 md:w-1/3 text-center mb-8">
-          <h4 className="text-3xl mb-4">{t("best_selling")}</h4>
-          <span>{t("best_selling_desc")}</span>
+      <section className="w-full h-full flex flex-col justify-center md:items-center mt-16 mb-20">
+        <div className="flex justify-center">
+          <div className="w-3/4 sm:w-1/2 md:w-1/3 text-center mb-8">
+            <h4 className="text-3xl mb-4">{t("best_selling")}</h4>
+            <span>{t("best_selling_desc")}</span>
+          </div>
         </div>
-        <div className="flex flex-col md:flex-row w-full px-6 sm:px-16 md:px-20">
-          <Card3
-            imgSrc1={currentItems[3].img1 as string}
-            imgSrc2={currentItems[3].img2 as string}
-            itemName={currentItems[3].name}
-            itemPrice={currentItems[3].price}
-            onClick={() => addOne!(currentItems[3])}
-            onAddWishlist={() => addToWishlist!(currentItems[3])}
-            itemLink={`/products/${encodeURIComponent(currentItems[3].id)}`}
-          />
-          <Card3
-            imgSrc1={currentItems[4].img1 as string}
-            imgSrc2={currentItems[4].img2 as string}
-            itemName={currentItems[4].name}
-            itemPrice={currentItems[4].price}
-            onAddWishlist={() => addToWishlist!(currentItems[4])}
-            onClick={() => addOne!(currentItems[4])}
-            itemLink={`/products/${encodeURIComponent(currentItems[3].id)}`}
-          />
-          <Card3
-            imgSrc1={currentItems[2].img1 as string}
-            imgSrc2={currentItems[2].img2 as string}
-            itemName={currentItems[2].name}
-            itemPrice={currentItems[2].price}
-            onAddWishlist={() => addToWishlist!(currentItems[2])}
-            onClick={() => addOne!(currentItems[2])}
-            itemLink={`/products/${encodeURIComponent(currentItems[3].id)}`}
-          />
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-x-4 lg:gap-x-12 gap-y-6 mb-10 px-4 sm:px-16 md:px-20">
+          <Card key={currentItems[3].id} item={currentItems[3]} />
+          <Card key={currentItems[4].id} item={currentItems[4]} />
+          <Card key={currentItems[2].id} item={currentItems[2]} />
+          <Card key={currentItems[5].id} item={currentItems[5]} />
         </div>
       </section>
       <section className="w-full hidden h-full py-16 md:flex flex-col items-center bg-lightgreen">
@@ -127,41 +115,32 @@ const Home: React.FC<Props> = ({ products }) => {
         <TestiSlider />
       </section>
 
-      <section className="px-2 sm:px-8 md:px-16 my-16 flex flex-col items-center">
+      <section className="px-4 sm:px-8 md:px-16 my-16 flex flex-col lg:items-center">
         <div className="text-center mb-6">
           <h4 className="text-3xl">{t("featured_products")}</h4>
         </div>
-        <div className="flex flex-col sm:flex-row flex-wrap w-full mb-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-4 gap-y-10 sm:gap-y-6 mb-10">
           {currentItems.map((item) => (
-            <Card5
-              key={item.id}
-              imgSrc1={item.img1 as string}
-              imgSrc2={item.img2 as string}
-              itemName={item.name}
-              itemPrice={item.price}
-              onAddWishlist={() => addToWishlist!(item)}
-              onClick={() => addOne!(item)}
-              itemLink={`/products/${encodeURIComponent(item.id)}`}
-            />
+            <Card key={item.id} item={item} />
           ))}
         </div>
-        <span
-          className="cursor-pointer"
-          onClick={() => setTotalItems((prevState) => prevState * 2)}
-        >
-          <Button value={t("see_more")} />
-        </span>
+        <div className="flex justify-center">
+          <Button
+            value={t("see_more")}
+            onClick={() => setTotalItems((prevState) => prevState * 2)}
+          />
+        </div>
       </section>
 
       <div className="border-gray100 border-b-2"></div>
 
       <section className="mt-16 mb-20 flex flex-col justify-center items-center text-center">
-        <div className="textBox w-2/5 mb-6">
+        <div className="textBox w-3/4 md:w-2/4 lg:w-2/5 mb-6">
           <h4 className="text-3xl mb-6">{t("our_shop")}</h4>
-          <span>{t("our_shop_desc")}</span>
+          <span className="w-full">{t("our_shop_desc")}</span>
         </div>
-        <div className="w-full px-20 flex justify-center">
-          <img className="w-full" src="/bg-img/ourshop.png" alt="Our Shop" />
+        <div className="w-full px-6 sm:px-20 flex justify-center">
+          <Image src={ourShop} alt="Our Shop" />
         </div>
       </section>
 
