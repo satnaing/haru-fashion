963f62c7ae04023f5bc3312878cede592216e9dc
Sat Naing <satnaingdev@gmail.com>
2022-02-20 23:21:25 +0630

    fix: wrap Image (Link) with a tag

--- pages/shopping-cart.tsx
+++ pages/shopping-cart.tsx
@@ -73,17 +73,16 @@ const ShoppingCart = () => {
                   return (
                     <tr className="border-b-2 border-gray200" key={item.id}>
                       <td className="my-3 flex flex-col xl:flex-row items-start sm:items-center xl:space-x-2 text-center xl:text-left">
-                        <Link
-                          href={`/products/${encodeURIComponent(item.id)}`}
-                          passHref
-                        >
-                          <Image
-                            src={item.img1 as string}
-                            alt={item.name}
-                            width={95}
-                            height={128}
-                            className="h-32 xl:mr-4"
-                          />
+                        <Link href={`/products/${encodeURIComponent(item.id)}`}>
+                          <a>
+                            <Image
+                              src={item.img1 as string}
+                              alt={item.name}
+                              width={95}
+                              height={128}
+                              className="h-32 xl:mr-4"
+                            />
+                          </a>
                         </Link>
                         <span>{item.name}</span>
                       </td>
