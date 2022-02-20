3a0b1a6bb5ca7bc2f88a0dcc8cafce42d17c4cfd
Sat Naing <satnaingdev@gmail.com>
2022-02-21 00:44:46 +0630

    seo: add default meta desc and keywords for head

--- components/Header/AppHeader.tsx
+++ components/Header/AppHeader.tsx
@@ -3,9 +3,20 @@ import React from "react";
 
 type Props = {
   title?: string;
+  desc?: string;
+  keywords?: string;
 };
 
-const AppHeader: React.FC<Props> = ({ title = "Haru Fashion" }) => (
+const defaultDesc =
+  "Discover affordable and fashionable men's and women's clothing online at Haru Fashion. Free Returns ✓ 1000+ New Arrivals Dropped Daily.";
+const defaultKeywords =
+  "Women's &amp; Men's Clothing, Shop Online Haru Fashion";
+
+const AppHeader: React.FC<Props> = ({
+  title = "Haru Fashion",
+  desc = defaultDesc,
+  keywords = defaultKeywords,
+}) => (
   <Head>
     <title>{title}</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -14,6 +25,8 @@ const AppHeader: React.FC<Props> = ({ title = "Haru Fashion" }) => (
       sizes="180x180"
       href="/favicons/apple-touch-icon.png"
     />
+    <meta name="description" content={desc}></meta>
+    <meta name="keywords" content={keywords}></meta>
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <link
