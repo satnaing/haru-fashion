8dafd3b2711841d3b841db41a36fa240a0781a65
Sat Naing <satnaingdev@gmail.com>
2022-02-20 20:47:19 +0630

    style: update OverlayContainer to be responsive

--- components/OverlayContainer/OverlayContainer.tsx
+++ components/OverlayContainer/OverlayContainer.tsx
@@ -11,19 +11,11 @@ const OverlayContainer: FC<Props> = ({ imgSrc, imgSrc2, imgAlt, children }) => (
   <div className={styles.imgContainer}>
     {imgSrc2 ? (
       <picture>
-        <source media="(min-width:992px)" srcSet={imgSrc} />
+        <source media="(min-width:576px)" srcSet={imgSrc} />
         <source media="(min-width:465px)" srcSet={imgSrc2} />
         <img className={styles.img} src={imgSrc2} alt={imgAlt} />
       </picture>
     ) : (
-      // <img
-      //   srcSet={`${imgSrc2} 480w,
-      //        ${imgSrc} 800w`}
-      //   sizes="(max-width: 600px) 480px,
-      //       800px"
-      //   // src={imgSrc}
-      //   alt="Elva dressed as a fairy"
-      // />
       <img className={styles.img} src={imgSrc} alt={imgAlt} />
     )}
 
