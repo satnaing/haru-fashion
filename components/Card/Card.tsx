fcfe84f4a20a419cc7d312ff0038496997ee8bc8
Sat Naing <satnaingdev@gmail.com>
2022-02-20 16:25:22 +0630

    style: update Card and card layout

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,61 +1,96 @@
-import { FC, MouseEventHandler } from "react";
+import { FC, useState, useContext } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { useTranslations } from "next-intl";
 
+import WishlistContext from "../../context/wishlist/WishlistContext";
 import Heart from "../../public/icons/Heart";
 import styles from "./Card.module.css";
+import HeartSolid from "../../public/icons/HeartSolid";
+import { itemType } from "../../context/cart/cart-types";
+import CartContext from "../../context/cart/CartContext";
 
 type Props = {
-  imgSrc1: string;
-  imgSrc2: string;
-  imgAlt?: string;
-  itemLink?: string;
-  itemName: string;
-  itemPrice: number;
-  onAddWishlist?: MouseEventHandler<HTMLButtonElement>;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
+  item: itemType;
 };
 
-const Card: FC<Props> = ({
-  imgSrc1,
-  imgSrc2,
-  imgAlt,
-  itemLink = "",
-  itemName,
-  itemPrice,
-  onAddWishlist,
-  onClick,
-}) => {
+const Card: FC<Props> = ({ item }) => {
   const t = useTranslations("CartWishlist");
+  const { wishlist, addToWishlist, deleteWishlistItem, clearWishlist } =
+    useContext(WishlistContext);
+  const { addOne } = useContext(CartContext);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isWLHovered, setIsWLHovered] = useState(false);
+
+  const { id, name, price, img1, img2 } = item;
+
+  const itemLink = `/products/${encodeURIComponent(id)}`;
+
+  const alreadyWishlisted =
+    wishlist.filter((wItem) => wItem.id === id).length > 0;
+
+  const handleWishlist = () => {
+    alreadyWishlisted ? deleteWishlistItem!(item) : addToWishlist!(item);
+  };
 
   return (
-    <div className={styles.cardContainer}>
+    <div
+      className={styles.card}
+      onMouseOver={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className={styles.imageContainer}>
-        <Link href={itemLink}>
-          <a>
-            <img className={styles.firstImage} src={imgSrc1} alt={imgAlt} />
-            <img className={styles.secondImage} src={imgSrc2} alt={imgAlt} />
-          </a>
-        </Link>
+        {!isHovered && (
+          <Image
+            src={img1 as string}
+            alt={name}
+            width={230}
+            height={300}
+            layout="responsive"
+          />
+        )}
+        {isHovered && (
+          <Image
+            className="transition-transform transform hover:scale-110 duration-1000"
+            src={img2 as string}
+            alt={name}
+            width={230}
+            height={300}
+            layout="responsive"
+          />
+        )}
         <button
           type="button"
+          className="absolute top-2 right-2 p-1 rounded-full"
           aria-label="Wishlist"
-          onClick={onAddWishlist}
-          className={styles.wishlist}
+          onClick={handleWishlist}
+          onMouseOver={() => setIsWLHovered(true)}
+          onMouseLeave={() => setIsWLHovered(false)}
         >
-          <Heart />
+          {isWLHovered || alreadyWishlisted ? <HeartSolid /> : <Heart />}
         </button>
-        <div className="absolute z-20 bottom-5 left-1/2 w-4/5 translate-y-28 "></div>
-        <div className={styles.btnWrapper}>
-          <button type="button" onClick={onClick} className={styles.addBtn}>
-            {t("add_to_cart")}
-          </button>
-        </div>
+        <button
+          type="button"
+          onClick={() => addOne!(item)}
+          className={styles.addBtn}
+        >
+          {t("add_to_cart")}
+        </button>
       </div>
-      <Link href={itemLink}>
-        <a className={styles.itemName}>{itemName}</a>
-      </Link>
-      <span className="text-gray400">$ {itemPrice}</span>
+
+      <div className="content">
+        <Link href={itemLink}>
+          <a className={styles.itemName}>{name}</a>
+        </Link>
+        <div className="text-gray400">$ {price}</div>
+        <button
+          type="button"
+          onClick={() => addOne!(item)}
+          className="uppercase font-bold text-sm sm:hidden"
+        >
+          {t("add_to_cart")}
+        </button>
+      </div>
     </div>
   );
 };
