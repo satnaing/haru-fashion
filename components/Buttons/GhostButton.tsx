a53d455d4ca4bb012b08792ffb7749992bb618dc
Sat Naing <satnaingdev@gmail.com>
2022-02-21 22:26:48 +0630

    a11y: update GhostButton with semantic html and ourline

--- components/Buttons/GhostButton.tsx
+++ components/Buttons/GhostButton.tsx
@@ -2,52 +2,43 @@ import { FC, forwardRef } from "react";
 
 type Props = {
   extraClass?: string;
-  size?: "sm" | "lg" | "xl";
-  value: string;
-  inverted?: true;
+  size?: "sm" | "normal" | "lg";
+  inverted?: boolean;
   noBorder?: boolean;
-  onClick?: React.MouseEventHandler<HTMLAnchorElement>;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 // eslint-disable-next-line react/display-name
-const GhostButton: FC<Props> = forwardRef(
-  (
-    {
-      size = "sm",
-      value,
-      extraClass,
-      inverted = false,
-      noBorder = false,
-      onClick,
-      children,
-    },
-    ref
-  ) => {
-    let btnSize = "";
-    if (size === "sm") {
-      btnSize = "py-2 sm:py-1 px-5";
-    } else if (size === "lg") {
-      btnSize = "py-3 sm:py-2 px-6";
-    } else {
-      btnSize = "py-4 sm:py-3 px-7  text-xl";
-    }
-    return (
-      <div
-        className={`${extraClass} bg-white group inline-block cursor-pointer ${
-          noBorder ? "" : "border border-gray500"
-        }  ${inverted && "hover:bg-gray500"}`}
-      >
-        <a
-          onClick={onClick}
-          className={` ${
-            inverted ? "group-hover:text-white" : "group-hover:text-gray400"
-          } no-underline text-xl sm:text-base inline-block text-gray500 tracking-widest ${btnSize}`}
-        >
-          {value} <span className="ml-1">{children}</span>
-        </a>
-      </div>
-    );
+const GhostButton: FC<Props> = ({
+  onClick,
+  size,
+  extraClass,
+  noBorder = false,
+  inverted = true,
+  children,
+}) => {
+  let btnSize = "";
+  if (size === "sm") {
+    btnSize = "py-2 sm:py-1 px-5";
+  } else if (size === "lg") {
+    btnSize = "py-4 sm:py-3 px-7  text-xl";
+  } else {
+    btnSize = "py-3 sm:py-2 px-6";
   }
-);
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`bg-white text-center cursor-pointer text-xl sm:text-base tracking-widest text-gray500 ${
+        !noBorder && "border border-gray500"
+      } ${
+        inverted ? "hover:bg-gray500 hover:text-gray100" : "hover:text-gray400"
+      } ${btnSize} ${extraClass}`}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default GhostButton;
