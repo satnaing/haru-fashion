9f125bf0612a0805493f5ca1a97c433724325f8b
Sat Naing <satnaingdev@gmail.com>
2022-02-23 11:15:09 +0630

    seo: update meta tags in _document

--- components/Header/AppHeader.tsx
+++ components/Header/AppHeader.tsx
@@ -21,57 +21,17 @@ const AppHeader: React.FC<Props> = ({
   return (
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
 
-      <meta content={desc} name="description" />
-      <meta content={keywords} name="keywords" />
-
-      <meta content="follow, index" name="robots" />
-      <meta content="#282828" name="theme-color" />
-      <meta content="#282828" name="msapplication-TileColor" />
-
-      <link
-        href="/favicons/apple-touch-icon.png"
-        rel="apple-touch-icon"
-        sizes="180x180"
-      />
-      <link
-        href="/favicons/favicon-32x32.png"
-        rel="icon"
-        sizes="32x32"
-        type="image/png"
-      />
-      <link
-        href="/favicons/favicon-16x16.png"
-        rel="icon"
-        sizes="16x16"
-        type="image/png"
-      />
-      <link href="/favicons/favicon.ico" rel="shortcut icon" />
-      <link href="/favicons/site.webmanifest" rel="manifest" />
-
-      <meta property="og:url" content="https://haru-fashion.vercel.app" />
-      <link rel="canonical" href="https://haru-fashion.vercel.app" />
-      <meta property="og:site_name" content="Haru Fashion" />
-      <meta property="og:description" content={desc} />
-      <meta property="og:title" content={title} />
+      <meta content={desc} name="description" key="description" />
+      <meta content={keywords} name="keywords" key="keywords" />
+      <meta property="og:description" content={desc} key="og_description" />
+      <meta property="og:title" content={title} key="og_title" />
+      <meta name="twitter:title" content={title} key="twitter_title" />
       <meta
-        property="og:image"
-        content="https://haru-fashion.vercel.app/og.png"
+        name="twitter:description"
+        content={desc}
+        key="twitter_description"
       />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@satnaing.dev" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={desc} />
-      <meta
-        name="twitter:image"
-        content="https://haru-fashion.vercel.app/og.png"
-      />
-
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     </Head>
   );
 };
---
+++ pages/_document.tsx
@@ -0,0 +1,73 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const title = "Haru Fashion";
+const desc =
+  "Haru Fashion e-commerce developed with Next.JS. Coded with 🖤 by Sat Naing (satnaing.dev).";
+const keywords = "Haru Fashion, Online Shop, E-commerce, Sat Naing, NextJS";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+        <meta content={desc} name="description" key="description" />
+        <meta content={keywords} name="keywords" key="keywords" />
+
+        <meta content="follow, index" name="robots" />
+        <meta content="#282828" name="theme-color" />
+        <meta content="#282828" name="msapplication-TileColor" />
+
+        <link
+          href="/favicons/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/favicons/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/favicons/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link href="/favicons/favicon.ico" rel="shortcut icon" />
+        <link href="/favicons/site.webmanifest" rel="manifest" />
+
+        <meta property="og:url" content="https://haru-fashion.vercel.app" />
+        <link rel="canonical" href="https://haru-fashion.vercel.app" />
+        <meta property="og:site_name" content="Haru Fashion" />
+        <meta property="og:description" content={desc} key="og_description" />
+        <meta property="og:title" content={title} key="og_title" />
+        <meta
+          property="og:image"
+          content="https://haru-fashion.vercel.app/og.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@satnaing.dev" />
+        <meta name="twitter:title" content={title} key="twitter_title" />
+        <meta
+          name="twitter:description"
+          content={desc}
+          key="twitter_description"
+        />
+        <meta
+          name="twitter:image"
+          content="https://haru-fashion.vercel.app/og.png"
+        />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
