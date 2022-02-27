1891961d4c700d4b591f135ff04b257d927cc9b3
Sat Naing <satnaingdev@gmail.com>
2022-02-27 14:29:00 +0630

    style: add loading component

---
+++ public/icons/Loading.tsx
@@ -0,0 +1,12 @@
+import styles from "./Loading.module.css";
+
+const Loading = () => (
+  <div className={styles.ldsEllipsis}>
+    <div></div>
+    <div></div>
+    {/* <div></div> */}
+    <div></div>
+  </div>
+);
+
+export default Loading;
