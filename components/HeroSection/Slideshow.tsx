0dcd3660377bd11bfe4f7a7ec676f955c25362e5
Sat Naing <satnaingdev@gmail.com>
2022-03-05 01:48:21 +0630

    style: replace react-slideshow with swiperjs for carousel

--- components/HeroSection/Slideshow.tsx
+++ components/HeroSection/Slideshow.tsx
@@ -1,31 +1,44 @@
-import { Slide } from "react-slideshow-image";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 
-import LeftArrow from "../../public/icons/LeftArrow";
-import RightArrow from "../../public/icons/RightArrow";
 import TextButton from "../Buttons/TextButton";
 import styles from "./Hero.module.css";
 
+// swiperjs
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import Swiper core and required modules
+import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper/core";
+
+// install Swiper modules
+SwiperCore.use([Pagination, Navigation, Autoplay]);
+
 const sliders = [
   {
-    id: 1,
-    bgImg: "/bg-img/curly_hair_white-1.jpg",
-    subtitle: "Spring Revolution",
-    titleUp: "Night Summer",
-    titleDown: "Dresses",
-    rightText: true,
-  },
-  {
     id: 2,
-    bgImg: "/bg-img/curly_hair_girl-1.jpg",
+    image: "/bg-img/curly_hair_girl-1.jpg",
+    imageTablet: "/bg-img/curly_hair_girl-1-tablet.png",
+    imageMobile: "/bg-img/curly_hair_girl-1_mobile.jpg",
     subtitle: "50% off",
     titleUp: "New Cocktail",
     titleDown: "Dresses",
     rightText: false,
   },
   {
+    id: 1,
+    image: "/bg-img/curly_hair_white-1.jpg",
+    imageTablet: "/bg-img/curly_hair_white-1-tablet.png",
+    imageMobile: "/bg-img/curly_hair_white-1_mobile.jpg",
+    subtitle: "Spring Revolution",
+    titleUp: "Night Summer",
+    titleDown: "Dresses",
+    rightText: true,
+  },
+  {
     id: 3,
-    bgImg: "/bg-img/monigote.jpg",
+    image: "/bg-img/monigote.jpg",
+    imageTablet: "/bg-img/monigote-tablet.png",
+    imageMobile: "/bg-img/monigote_mobile.jpg",
     subtitle: "Spring promo",
     titleUp: "The Weekend",
     titleDown: "Promotions",
@@ -33,75 +46,80 @@ const sliders = [
   },
 ];
 
-const sectionHeight = "720px";
-
-const slideProperties = {
-  duration: 5000,
-  canSwipe: true,
-  prevArrow: (
-    <div
-      role="button"
-      title="Previous"
-      className={`${styles.arrows} left-2 md:left-8`}
-    >
-      <LeftArrow />
-    </div>
-  ),
-  nextArrow: (
-    <div
-      role="button"
-      title="Next"
-      className={`${styles.arrows} right-2 md:right-8`}
-    >
-      <RightArrow />
-    </div>
-  ),
-
-  // indicators: true,
-};
-
 const Slideshow = () => {
   const t = useTranslations("Index");
 
   return (
-    <div style={{ height: 640 }}>
-      <div className="slide-container absolute w-full h-screen top-0 lg:top-8 z-20">
-        <Slide {...slideProperties}>
+    <>
+      <div className="relative -top-20 slide-container w-full z-20">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={0}
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          navigation={true}
+          pagination={{
+            clickable: true,
+            type: "fraction",
+            dynamicBullets: true,
+          }}
+          className="mySwiper"
+        >
           {sliders.map((slider) => (
-            <div className="each-slide relative" key={slider.id}>
-              <div
-                style={{
-                  backgroundImage: `url(${slider.bgImg})`,
-                  height: sectionHeight,
-                  backgroundPosition: "50% 50%",
-                  backgroundSize: "cover",
-                }}
-              >
-                <div
-                  className={
-                    slider.rightText
-                      ? styles.rightTextSection
-                      : styles.leftTextSection
-                  }
-                >
-                  <span className={styles.subtitle}>{slider.subtitle}</span>
-                  <h1
-                    className={`${styles.title} ${
-                      slider.rightText ? "text-right" : "text-left"
-                    }`}
-                  >
-                    {slider.titleUp} <br />
-                    {slider.titleDown}
-                  </h1>
-                  <TextButton value={t("shop_now")} />
-                </div>
+            <SwiperSlide key={slider.id}>
+              <div className="hidden lg:block">
+                <Image
+                  layout="responsive"
+                  src={slider.image}
+                  width={1144}
+                  height={572}
+                  alt={"some name"}
+                />
               </div>
-            </div>
+              <div className="hidden sm:block lg:hidden">
+                <Image
+                  layout="responsive"
+                  src={slider.imageTablet}
+                  width={820}
+                  height={720}
+                  alt={"some name"}
+                />
+              </div>
+              <div className="sm:hidden">
+                <Image
+                  layout="responsive"
+                  src={slider.imageMobile}
+                  width={428}
+                  height={800}
+                  alt={"some name"}
+                />
+              </div>
+              <div
+                className={
+                  slider.rightText
+                    ? styles.rightTextSection
+                    : styles.leftTextSection
+                }
+              >
+                <span className={styles.subtitle}>{slider.subtitle}</span>
+                <span
+                  className={`${styles.title} text-center ${
+                    slider.rightText ? "sm:text-right" : "sm:text-left"
+                  }`}
+                >
+                  {slider.titleUp} <br />
+                  {slider.titleDown}
+                </span>
+                <TextButton value={t("shop_now")} />
+              </div>
+            </SwiperSlide>
           ))}
-        </Slide>
+        </Swiper>
       </div>
-      {/* <div style={{ height: sectionHeight, zIndex: -1 }}></div> */}
-    </div>
+    </>
   );
 };
 
